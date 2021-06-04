8984039b6baa82526a2f6b17ac4b1a33a1d446d1
Nathanaël Renaud <67143274+0xNath@users.noreply.github.com>
2021-06-05 00:06:05 +0200

    Add

---
+++ backup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+BACKUP_NAME="$(date +"%Y_%m_%d_%I_%M")"
+TARGETS=('/home' '/var/log')
+BACKUP_ROOT='./backup'
+BACKUP_DIR="${BACKUP_ROOT}/${BACKUP_NAME}"
+
+if test -f "./pass"; then
+  PASS="$(cat ./pass)"
+  echo "Password file found."
+else
+	echo 'Password file not found. Generating one to "./pass".'
+	gpg --gen-random 2 32 > ./pass
+	PASS="$(cat ./pass)"
+	chmod 400 ./pass
+fi
+echo ""
+
+if [ "$1" == "-d" ]; then
+	gpg --yes --batch --decrypt --passphrase=$PASS $2 | tar xz --strip-components 2
+	exit
+else
+	mkdir $BACKUP_ROOT
+fi
+
+read -p 'FTP hostname : ' FTP_HOST
+
+read -p '	╚ username : ' FTP_USERNAME
+
+read -s -p '	╚ password : ' FTP_PASS
+echo ""
+
+mkdir -p $BACKUP_DIR
+
+
+for i in ${TARGETS[@]}; do
+	echo ""
+	echo "$(date "+%D %R") : backing up $i ..."
+	if sudo rsync -Ra $i ${BACKUP_DIR}; then
+		echo "	╚ $(date "+%D %R") : Successfully backed up $i"
+	else
+		echo "	╚ $(date "+%D %R") : Failed backing up $i"
+	fi
+done
+
+echo ""
+echo "$(date "+%D %R") : Beginning compression and encryption of ${BACKUP_DIR}"
+sudo tar -zcf "${BACKUP_DIR}.tar.gz" ${BACKUP_DIR}
+gpg --symmetric --yes --batch --cipher-algo AES256 --compress-algo none --passphrase="$PASS" ${BACKUP_DIR}.tar.gz
+echo ""
+echo "$(date "+%D %R") : Successfully compressed ${BACKUP_DIR}"
+
+echo ""
+echo "$(date "+%D %R") : Beginning transfert of backup"
+echo ""
+ftp -inv $FTP_HOST << EOF
+user $FTP_USERNAME $FTP_PASS
+binary
+lcd $BACKUP_ROOT
+put $BACKUP_NAME.tar.gz.gpg
+bye
+EOF
+echo ""
+sudo rm -Rf $BACKUP_ROOT
+echo "$(date "+%D %R") : Job finish"
