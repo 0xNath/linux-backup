fafe76150fb0ead43ae458839168f7133e47ab69
0xNath <nathanael.renaud86@gmail.com>
2022-01-02 15:34:19 +0100

    Fix bugs ...

--- backup.sh
+++ backup.sh
@@ -5,29 +5,29 @@ TARGETS=('/home' '/etc/' '/var/log')
 BACKUP_ROOT='./backup'
 BACKUP_DIR="${BACKUP_ROOT}/${BACKUP_NAME}"
 
-if test -f "./pass"; then
-  PASS="$(cat ./pass)"
+if test -f "./.pass"; then
+  PASS="$(cat ./.pass)"
   echo "Password file found."
 else
-	echo 'Password file not found. Generating one to "./pass".'
-	gpg --gen-random 2 32 > ./pass
-	PASS="$(cat ./pass)"
-	chmod 400 ./pass
+        echo 'Password file not found. Generating one to "./.pass".'
+        gpg -a --gen-random 2 256 > ./.pass
+        PASS="$(cat ./.pass)"
+        chmod 400 ./.pass
 fi
 echo ""
 
 if [ "$1" == "-d" ]; then
-	gpg --yes --batch --decrypt --passphrase=$PASS $2 | tar xz --strip-components 2
-	exit
+        gpg --yes --batch --decrypt --cipher-algo "AES256" --passphrase="$PASS" $2 | tar xz 
+        exit
 else
-	mkdir $BACKUP_ROOT &>/dev/null
+        mkdir $BACKUP_ROOT &>/dev/null
 fi
 
 read -p 'FTP hostname : ' FTP_HOST
 
-read -p '	╚ username : ' FTP_USERNAME
+read -p '    ╚ username : ' FTP_USERNAME
 
-read -s -p '	╚ password : ' FTP_PASS
+read -s -p '    ╚ password : ' FTP_PASS
 echo ""
 
 LOGIN_ATTEMPT="$(ftp -inv $FTP_HOST << EOF
@@ -37,30 +37,29 @@ EOF
 )"
 
 if [[ "$(echo $LOGIN_ATTEMPT)" =~ "User logged in" ]]; then
-	echo ""
-	echo "login success"
+        echo ""
+        echo "login success"
 else
-	echo ""
-	echo "login attempt failed"
-	exit
+        echo ""
+        echo "login attempt failed"
+        exit
 fi
 
 mkdir -p $BACKUP_DIR &>/dev/null
 
 for i in ${TARGETS[@]}; do
-	echo ""
-	echo "$(date "+%D %R") : backing up $i ..."
-	if sudo rsync --exclude "$(cd "$(dirname "$BACKUP_DIR")"; pwd)/$(basename "$BACKUP_DIR")" -Ra $i ${BACKUP_DIR}; then
-		echo "	╚ $(date "+%D %R") : Successfully backed up $i"
-	else
-		echo "	╚ $(date "+%D %R") : Failed backing up $i"
-	fi
+        echo ""
+        echo "$(date "+%D %R") : backing up $i ..."
+        if sudo rsync --exclude "$(cd "$(dirname "$BACKUP_DIR")"; pwd)/$(basename "$BACKUP_DIR")" -Ra $i ${BACKUP_DIR}; then
+                echo "  ╚ $(date "+%D %R") : Successfully backed up $i"
+        else
+                echo "  ╚ $(date "+%D %R") : Failed backing up $i"
+        fi
 done
 
 echo ""
 echo "$(date "+%D %R") : Beginning compression and encryption of ${BACKUP_DIR}"
-sudo tar -zcf "${BACKUP_DIR}.tar.gz" ${BACKUP_DIR}
-gpg --symmetric --yes --batch --cipher-algo AES256 --compress-algo none --passphrase="$PASS" ${BACKUP_DIR}.tar.gz
+sudo tar -zc ${BACKUP_DIR} | gpg --symmetric --yes --batch --cipher-algo "AES256" --compress-algo none --passphrase="$PASS" -o "$BACKUP_NAME.tar.gz.gpg"
 echo ""
 echo "$(date "+%D %R") : Successfully compressed ${BACKUP_DIR}"
 
@@ -70,10 +69,10 @@ echo ""
 ftp -inv $FTP_HOST << EOF
 user $FTP_USERNAME $FTP_PASS
 binary
-lcd $BACKUP_ROOT
 put $BACKUP_NAME.tar.gz.gpg
 bye
 EOF
 echo ""
+sudo rm -Rf $BACKUP_NAME.tar.gz.gpg
 sudo rm -Rf $BACKUP_ROOT
 echo "$(date "+%D %R") : Job finish"
